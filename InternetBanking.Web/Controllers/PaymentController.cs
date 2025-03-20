6604ee87086536eac766b2215f9db6f61bec8298
FrancisEncarnacion <francisencarnacion082@gmail.com>
2025-03-20 16:02:56 -0400

    Modificacion de la vista HOME y creacion de las vistas para pagos

---
+++ InternetBanking.Web/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetBanking.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
