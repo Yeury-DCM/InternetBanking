7e49136a061c14ae599d483ba7ae368aad157da3
Yeury_dcm <yeury.dcm2406@gmail.com>
2025-03-25 23:44:05 -0400

    Prueba cambios

--- InternetBanking.Web/Controllers/BeneficiaryController.cs
+++ InternetBanking.Web/Controllers/BeneficiaryController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InternetBanking.Core.Application.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InternetBanking.Web.Controllers
 {
+    [Authorize(Roles = "Client")]
     public class BeneficiaryController : Controller
     {
         public IActionResult Index()
